0f5513abeb2342a495c3f22110b796ce33573f7a
Akhil Bharti <akhil.bh96@gmail.com>
2021-12-29 16:47:44 +0530

    buyout related test cases

--- contracts/Twav/Twav.sol
+++ contracts/Twav/Twav.sol
@@ -30,7 +30,7 @@ contract Twav {
         lastBlockTimeStamp = _blockTimestamp;
     }
 
-    function _getTwav() internal view returns(uint256 _twav){
+    function _getTwav() public view returns(uint256 _twav){
         uint8 _index = ((twavObservationsIndex + TWAV_BLOCK_NUMBERS) - 1) % TWAV_BLOCK_NUMBERS;
         TwavObservation memory _twavObservationCurrent = twavObservations[(_index)]; //to subtract 1 from current index
         TwavObservation memory _twavObservationPrev = twavObservations[(_index + 1) % TWAV_BLOCK_NUMBERS];
